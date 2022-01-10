474a27cf85bb0be2a8f534a9e1f011ec1bae31b9
vinicius falconi <vinicius.falconi@ahgora.com.br>
2022-01-10 16:52:02 -0300

    fixing to support .app

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 //Afastamentos 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/afastamentos' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/afastamentos')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/afastamentos.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -34,7 +34,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 //Apuracao
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/batidas/apuracao' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/batidas/apuracao')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/apuracao.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -46,7 +46,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Batida Online
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/batidaonline/dispositivos' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/batidaonline/dispositivos')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/batidaOnline.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -58,7 +58,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Bloqueio de competências
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('https://www.ahgora.com.br/bloqueio')) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/bloqueio')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/bloqueio.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -70,7 +70,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Folha de pagamento
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('https://www.ahgora.com.br/exporta')) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/exporta')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/folhaDePagamento.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -82,7 +82,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // AFD dos dispositivos
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('https://www.ahgora.com.br/afds/gerar_afds')) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/afds/gerar_afds')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/afds.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -94,7 +94,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Gerar Documentos
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/documentos' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/documentos')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/documentos.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -106,7 +106,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Funcionários
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/funcionarios' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/funcionarios')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/funcionario.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -118,7 +118,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Feriados
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/feriados' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/feriados')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/feriados.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -130,7 +130,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Gerenciador de Espelhos
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/gerenciador_espelhos' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/gerenciador_espelhos')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/gerenciadorEspelhos.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -142,7 +142,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Justificativas
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/justificativa' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/justificativa')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/justificativas.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -154,7 +154,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Mapa de batidas area de atuacao
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/cercas' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/cercas')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/areaAtuacao.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -167,7 +167,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 // Horários
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('https://www.ahgora.com.br/horarios')) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/horarios')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/horarios.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -179,7 +179,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Importação
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('https://www.ahgora.com.br/importa')) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/importa')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/importacao.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -191,7 +191,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Relatórios
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/relatorios' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/relatorios')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/relatorios.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -203,7 +203,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Comunicados
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/notificationPolicy' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/notificationPolicy')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/comunicados.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -215,7 +215,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Relatorio batidas impares
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/relatorios/batidas_impares' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/relatorios/batidas_impares')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/relatorioBatidasImpar.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -227,7 +227,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Relatorio batidas por periodo
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/relatorios/batidas_periodo' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/relatorios/batidas_periodo')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/relatorioBatidasPeriodo.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
@@ -239,7 +239,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // Relatorio sumarizado diario por funcionario
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && 'https://www.ahgora.com.br/relatorios/completo/irr' === tab.url) {
+    if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined && tab.url.includes('ahgora.com.br/relatorios/completo/irr')) {
         chrome.tabs.executeScript(tabId, {file: 'pages/relatorioSumarizadoDiario.js'}, () => {
             chrome.storage.local.get('state', value => {
                 chrome.tabs.sendMessage(tabId, value)
